3b01fcb3b1fd64e42548d8c1948c5123025f4471
Angel17ms <morenosuarezangel@gmail.com>
2023-10-30 18:49:08 +0000

    guardado

--- clash/models/models.py
+++ clash/models/models.py
@@ -3,95 +3,85 @@
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
 
-
-class player(models.Model):
+class Player(models.Model):
     _name = 'clash.player'
     _description = 'Player of Clash of War'
 
     name = fields.Char()
-    level = fields.Integer(default = 1)
+    level = fields.Integer(default=1)
+    village_id = fields.Many2one('clash.village', string='Village')
 
-
-class village(models.Model):
+class Village(models.Model):
     _name = 'clash.village'
-    _description = 'Village of Clash of War'
+    _description = 'Village in Clash of War'
 
     name = fields.Char()
-    city_hall_level = fields.Integer(default = 1)
-    resoureces = fields.One2many('clash.resource', 'aldea')
-    build = fields.One2many('clash.build', 'city')
-    defenses = fields.One2many('clash.defenses', 'city')
+    city_hall_level = fields.Integer(default=1)
+    resources = fields.One2many('clash.resource', 'village_id')
+    buildings = fields.One2many('clash.building', 'village_id')
+    defenses = fields.One2many('clash.defense', 'village_id')
 
-    
-
-class resource(models.Model):
+class Resource(models.Model):
     _name = 'clash.resource'
-    _description = 'Resources of Clash of War'
+    _description = 'Resources in Clash of War'
 
     name = fields.Char()
-    type = fields.Selection([('1', 'Gold'), ('2', 'Mana'), ('3', 'Gems')])
-    aldea = fields.Many2one('clash.village')
+    type = fields.Selection([('1', 'Gold'), ('2', 'Mana'), ('3', 'Gems')], string='Resource Type')
+    village_id = fields.Many2one('clash.village', string='Village')
 
-
-
-class building_types(models.Model):
-    _name = 'clash.building_types'
-    _description = 'Types of build in Clash of War'
+class BuildingType(models.Model):
+    _name = 'clash.building_type'
+    _description = 'Building Types in Clash of War'
 
     name = fields.Char()
-    gold_production = fields.Float()
-    mana_production = fields.Float()
-    gems_production = fields.Float()
-    trops_max = fields.Integer()
+    gold_production = fields.Float(string='Gold Production')
+    mana_production = fields.Float(string='Mana Production')
+    gems_production = fields.Float(string='Gems Production')
     health = fields.Float()
 
-
-class build(models.Model):
-    _name = 'clash.build'
-    _description = 'Builds of Clash of War'
+class Building(models.Model):
+    _name = 'clash.building'
+    _description = 'Buildings in Clash of War'
 
     name = fields.Char()
-    type = fields.Many2one('clash.building_types')
-    city = fields.Many2one('clash.village')
-    trops = fields.One2many('clash.trops_type','campament')
+    type_id = fields.Many2one('clash.building_type', string='Building Type')
+    village_id = fields.Many2one('clash.village', string='Village')
+    troops = fields.One2many('clash.troop_type', 'camp_id')
+    troops_max = fields.Integer(string='Maximum Troops Capacity')
     level = fields.Integer()
 
-
-class defenses_type(models.Model):
-    _name = 'clash.defenses_type'
-    _description = 'Types of defenses in Clash of War'
+class DefenseType(models.Model):
+    _name = 'clash.defense_type'
+    _description = 'Defense Types in Clash of War'
 
     name = fields.Char()
     damage = fields.Float()
     health = fields.Float()
 
-
-class defenses(models.Model):
-    _name = 'clash.defenses'
+class Defense(models.Model):
+    _name = 'clash.defense'
     _description = 'Defenses in Clash of War'
 
     name = fields.Char()
-    type = fields.Many2one('clash.defenses_type')
-    city = fields.Many2one('clash.village')
+    type_id = fields.Many2one('clash.defense_type', string='Defense Type')
+    village_id = fields.Many2one('clash.village', string='Village')
     level = fields.Integer()
 
-
-class trops_type(models.Model):
-    _name = 'clash.trops_type'
-    _description = 'Types of trops in Clash of War'
+class TroopType(models.Model):
+    _name = 'clash.troop_type'
+    _description = 'Troop Types in Clash of War'
 
     name = fields.Char()
     damage = fields.Float()
     health = fields.Float()
-    cost_of_production = fields.Float()
-    number_of_troops = fields.Integer()
-    campament = fields.Many2one('clash.build')
+    cost_of_production = fields.Float(string='Production Cost')
+    number_of_troops = fields.Integer(string='Number of Troops')
+    camp_id = fields.Many2one('clash.building', string='Camp')
 
-    @api.constrains('number_of_troops', 'campament')
     def check_troop_limit(self):
         for troop_type in self:
-            max_troops = troop_type.campament.type.trops_max
-            total_troops = sum(t.number_of_troops for t in troop_type.campament.trops)
-            if total_troops + troop_type.number_of_troops > max_troops:
-                raise ValidationError('Total number of troops exceeds the camp limit.')
-
+            max_troops = troop_type.camp_id.troops_max
+            total_troops = troop_type.camp_id.troops.number_of_troops + troop_type.number_of_troops
+            raise ValidationError('Total number of troops exceeds the camp limit. Maximum allowed: %s' % total_troops)
+            if total_troops > max_troops:
+                raise ValidationError('Total number of troops exceeds the camp limit. Maximum allowed: %s' % max_troops)
